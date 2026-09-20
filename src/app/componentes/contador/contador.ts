8366cf040f2e35b7ae1b6f21f1919132addb070e
jrdy-source20 <jordy18dz@gmail.com>
2026-09-20 22:13:34 +0000

    Primer commit - proyecto de angular sobre un contador

---
+++ src/app/componentes/contador/contador.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-contador',
+    imports: [],
+    templateUrl: './contador.html',
+    styleUrl: './contador.scss'
+})
+export class Contador{}
